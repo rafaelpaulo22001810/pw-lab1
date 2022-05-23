10b090bdcbf8d7c5aaa30cf48b0e342b87e7c910
Rafael <a22001810@alunos.ulht.pt>
2022-05-23 13:20:04 +0100

    lab8

--- lab8/portfolio/urls.py
+++ lab8/portfolio/urls.py
@@ -4,6 +4,7 @@ from . import views
 app_name = 'portfolio'
 
 urlpatterns = [
+    path('', views.home_page_view, name='home'),
     path('home', views.home_page_view, name='home'),
     path('licenciatura', views.licenciatura_page_view, name='licenciatura'),
     path('projetos', views.projetos_page_view, name='projetos'),
